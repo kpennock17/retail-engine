14f6c1869e61fa8eb521c7a6b0f20bed4eeb790b
Kevin Pennock <kpennock@usf.edu>
2025-09-21 23:28:33 -0400

    step 6

--- cc_4.js
+++ cc_4.js
@@ -1,3 +1,4 @@
+//step 2
 let inventory = [
     {sku:"P-1",name:"Polo",category:"Clothes",price:"60",inventory:"50"},
     {sku:"C-2",name:"Cleats",category:"Sports",price:"100",inventory:"20"},
@@ -5,7 +6,7 @@ let inventory = [
     {sku:"N-4",name:"Nintendo",category:"Electronic",price:"500",inventory:"25"},
     {sku:"G-5",name:"Guitar",category:"Music",price:"200",inventory:"25"},
 ];
-
+// step 3
 discount = 0;
 for (let  product of inventory){
     switch (product.category){
@@ -28,7 +29,7 @@ for (let  product of inventory){
 
 }
 console.log(products)
-
+//step 4
 let extraDiscount;
 
 if (customerType === "Student") { extraDiscount = 0.05;} 
@@ -36,7 +37,7 @@ else if (customerType === "Senior") {extraDiscount = 0.07;}
 else {extraDiscount = 0;}
 
 console.log(extraDiscount);
-
+//step 5
 let subtotal = 0;
 
 for (let product of products){ 
@@ -48,3 +49,17 @@ for (let product of products){
     console.log(`Customer ${i} (${customerType}): $${finalTotal.toFixed(2)}`);
 
 console.log("Remaining inventory:", products);
+//step 6
+
+let sampleProduct = inventory[0];  
+for (let key in sampleProduct) {
+    console.log(`${key}: ${sampleProduct[key]}`)};
+let sampleProduct2 = inventory[1];  
+for (let key in sampleProduct) {
+    console.log(`${key}: ${sampleProduct[key]}`)};
+    let sampleProduct3 = inventory[2];  
+for (let key in sampleProduct) {
+    console.log(`${key}: ${sampleProduct[key]}`)};
+
+
+}
