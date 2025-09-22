15b02a724dc27bd74741d61aabe944e64f8792bc
Kevin Pennock <kpennock@usf.edu>
2025-09-21 22:37:59 -0400

    first

---
+++ cc_4.js
@@ -0,0 +1,9 @@
+let inventory = [
+    {sku:"T-001",name:"T-Shirt",category:"Clothes",price:"20",inventory:"100"},
+    {sku:"S-002",name:"Soccer Ball",category:"Sports",price:"30",inventory:"200"},
+    {sku:"C-003",name:"Converse",category:"Shoes",price:"60",inventory:"150"},
+    {sku:"Comp-004",name:"Computer",category:"Electronic",price:"750",inventory:"75"},
+    {sku:"P-005",name:"Piano",category:"Music",price:"500",inventory:"25"},
+];
+
+
